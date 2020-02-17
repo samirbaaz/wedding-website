7f9c1d17e6174dcaaeb5626f95b7132c2bfb72ce
samirbaaz <samirbaaz@users.noreply.github.com>
2020-02-17 01:31:02 -0800

    Point rsvp form to correct google sheet.

--- js/scripts.js
+++ js/scripts.js
@@ -210,25 +210,15 @@ $(document).ready(function () {
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
         e.preventDefault();
+
         var data = $(this).serialize();
+        console.log(data)
 
         $('#alert-wrapper').html(alert_markup('info', '<strong>Just a sec!</strong> We are saving your details.'));
 
-        if (MD5($('#invite_code').val()) !== 'b0e53b10c1f55ede516b240036b88f40'
-            && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
-            $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
-        } else {
-            $.post('https://script.google.com/macros/s/AKfycbzUqz44wOat0DiGjRV1gUnRf4HRqlRARWggjvHKWvqniP7eVDG-/exec', data)
-                .done(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html('');
-                    $('#rsvp-modal').modal('show');
-                })
-                .fail(function (data) {
-                    console.log(data);
-                    $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> There is some issue with the server. '));
-                });
-        }
+        $.post('https://docs.google.com/forms/d/e/1FAIpQLSd9g2Q7B71weurRXgEwfyIz-3OKPbAx59L0vlR4ERfI72V-dw/formResponse', data)
+        $('#alert-wrapper').html('');
+        $('#rsvp-modal').modal('show');
     });
 
 });
